10a2410bc622cfb22b000ab51528ccc178cedf2d
Praveen Matoria <praveen.matoria@gmail.com>
2024-02-01 19:21:07 +0530

    TicTacToe

---
+++ Pages/Portfolio/TicTacToe.razor.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Numerics;
+
+namespace Portfolio.Pages.Portfolio;
+
+public partial class TicTacToe (IJSRuntime JS)
+{
+    private string[] Board { get; set; } = ["", "", "", "", "", "", "", "", ""];
+    private string Player { get; set; } = "X";
+    private int[][] WinningCombos { get; set; } =
+    [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ];
+
+    private async Task SquareCliked(int idx)
+    {
+        Board[idx] = Player;
+        Player = Player == "X" ? "O" : "X";
+
+        foreach (int[] combo in WinningCombos)
+        {
+            int p1 = combo[0];
+            int p2 = combo[1];
+            int p3 = combo[2];
+            if (Board[p1] == String.Empty || Board[p2] == String.Empty || Board[p3] == String.Empty) continue;
+            if (Board[p1] == Board[p2] && Board[p2] == Board[p3] && Board[p1] == Board[p3])
+            {
+                string winner = Player == "X" ? "Player TWO" : "Player ONE";
+                await JS.InvokeVoidAsync("ShowSwal", winner);
+                ResetGame(Board);
+            }
+        }
+
+        if (Board.All(x => x != ""))
+        {
+            await JS.InvokeVoidAsync("ShowTie");
+            ResetGame(Board);
+        }
+    }
+    private static void ResetGame(string[] Board)
+    {
+        for (int i = 0; i < Board.Length; i++)
+        {
+            Board[i] = "";
+        }
+    }
+}
