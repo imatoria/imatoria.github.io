5acc1cab9d86671951d4885d3ba0dd09fce6825a
Praveen Matoria <praveen.matoria@gmail.com>
2024-02-06 21:27:07 +0530

    Page Title

--- BasePage.cs
+++ BasePage.cs
@@ -8,6 +8,8 @@ public class BasePage: ComponentBase
     [Inject]
     public required IJSRuntime JS { get; set; }
 
+    public string Title { get; set; } = "Praveen Matoria - ";
+
     protected override async Task OnInitializedAsync()
     {
         var DotNetHelper = DotNetObjectReference.Create(this);
