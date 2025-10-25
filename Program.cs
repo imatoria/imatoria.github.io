12c4204cfc44531f7f6b2964af3ea3468176ba4e
Praveen Matoria <codechk@gmail.com>
2025-10-25 23:45:06 +0530

    MAJOR BUILD ERROR FIX

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.EntityFrameworkCore;
 using PraveenMatoria;
 using PraveenMatoria.Database;
-using PraveenMatoria.Database.Services;
 using SqliteWasmHelper;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
